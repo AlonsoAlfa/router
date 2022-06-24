5724f304852a3737bd01ff696bf8c8e380bea9aa
Alonsoalfa <afaustoalonso@gmail.com>
2022-06-23 22:22:10 -0600

    Feat: That file has been added which has said desired functionality which was requested

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Main from './Components/Main';
+import './Components/style.css'
+
+import { Outlet, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+export default App;
